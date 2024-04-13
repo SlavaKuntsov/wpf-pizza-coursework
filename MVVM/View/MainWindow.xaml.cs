c9256f9e3ffc9d6e852ec42123fb424f040e903b
Slava <kuncovs19@gmail.com>
2024-04-14 02:29:19 +0300

    Delete custom cursor

--- MVVM/View/MainWindow.xaml.cs
+++ MVVM/View/MainWindow.xaml.cs
@@ -7,18 +7,9 @@ namespace Pizza
 {
 	public partial class MainWindow : Window
 	{
-		Cursor cursor;
-
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			Console.WriteLine("___________________________");
-
-			string cursorDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\Assets\\cursors";
-			cursor = new Cursor($"{cursorDirectory}\\hello_kitty2.cur");
-
-			Project.Cursor = cursor;
 		}
 
 		private void DragWindow_MouseDown(object sender, MouseButtonEventArgs e)
